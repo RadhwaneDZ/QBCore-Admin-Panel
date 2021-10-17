1b3979edf36f20bbe52ba4d5838e4781458856ff
aidanohart <44532573+aidanohartt@users.noreply.github.com>
2021-10-17 14:55:12 +0100

    Update page_navigationBar.php

--- includes/page_navigationBar.php
+++ includes/page_navigationBar.php
@@ -1,17 +1,21 @@
 <div class="scrollbar-sidebar">
 <div class="app-sidebar__inner">
     <ul class="vertical-nav-menu">
-        <li class="app-sidebar__heading">Dashboards</li>
+        <li class="app-sidebar__heading">Dashboard</li>
             <li>
-                <a href="index.php" class="mm-active">
+                <a href="index.php">
                     <i class="metismenu-icon pe-7s-note"></i>
                     Dashboard
                 </a>
 
-                <a href="login.php">
-                    <i class="metismenu-icon pe-7s-rocket"></i>
-                    Dashboard Example 2
+                <a href="allPlayers.php">
+                    <i class="metismenu-icon pe-7s-albums"></i>
+                    All Players
                 </a>
+
+                <li class="app-sidebar__heading">Players & Characters</li>
+
+
             </li>
         </ul>
     </div>
