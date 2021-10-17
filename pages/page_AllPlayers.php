5189e2cb0b7d9e8c24945413d19f266792c5ac10
aidanohart <44532573+aidanohartt@users.noreply.github.com>
2021-10-17 14:55:06 +0100

    Started working on all Players

---
+++ pages/page_AllPlayers.php
@@ -0,0 +1,58 @@
+<div class="app-main__outer">
+<div class="app-main__inner">
+<div class="app-page-title">
+<div class="page-title-wrapper">
+    <div class="page-title-heading">
+        <div class="page-title-icon">
+            <i class="pe-7s-albums icon-gradient bg-qb-core">
+            </i>
+        </div>
+        <div>All players
+            <div class="page-title-subheading">This page shows a table of every player that has played on the server and you can view their profile.
+            </div>
+        </div>
+    </div>  
+</div>
+</div> 
+
+<div class="col-lg-70">
+<div class="main-card mb-3 card">
+    <div class="card-body"><h5 class="card-title">All Players</h5>
+        <table class="mb-0 table table-hover">
+            <thead>
+            <tr>
+                <th>Rockstar License</th>
+                <th>Column Name</th>
+                <th>Column Name</th>
+                <th>Column Name</th>
+                <th>Column Name</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>Mark</td>
+                <td>@mdo</td>
+                <td>Text here to test</td>
+                <td>Text here to test</td>
+                <td>Text here to test</td>
+
+            </tr>
+            <tr>
+                <td>Jacob</td>
+                <td>@fat</td>
+                <td>Text here to test</td>
+                <td>Text here to test</td>
+                <td>Text here to test</td>
+            </tr>
+            <tr>
+                <td>Larry</td>
+                <td>@twitter</td>
+                <td>Text here to test</td>
+                <td>Text here to test</td>
+                <td>Text here to test</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+</div>
