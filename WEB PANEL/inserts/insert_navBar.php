34f9ad4431fbf0c8127fab5d8cb29e29189f5745
aidanohart <44532573+aidanohartt@users.noreply.github.com>
2021-10-18 22:33:36 +0100

    Created separate files for all inserts for global use

---
+++ WEB PANEL/inserts/insert_navBar.php
@@ -0,0 +1,10 @@
+<div class="scrollbar-sidebar">
+    <div class="app-sidebar__inner">
+        <ul class="vertical-nav-menu">
+        <li class="app-sidebar__heading">General</li>
+            <li><a href="index.php" class="mm-active"><i class="metismenu-icon pe-7s-rocket"></i>Dashboard</a></li>
+            <li class="app-sidebar__heading">Player Management</li>
+            <li><a href="characterSearch.php"><i class="metismenu-icon pe-7s-rocket"></i>Search Characters</a></li>
+        </div>
+    </div>
+</div>   
---
+++ WEB PANEL/inserts/insert_profile.php
@@ -0,0 +1,31 @@
+<div class="app-header-right">
+    <div class="header-btn-lg pr-0">
+        <div class="widget-content p-0">
+            <div class="widget-content-wrapper">
+                <div class="widget-content-left"> </div>
+
+                <div class="widget-content-left  ml-3 header-user-info">
+                    <div class="widget-subheading">
+                        Logged in as: <b>aidanohart#0001</b>
+                    </div>
+                </div>
+
+                <div class="btn-group">
+                        <a data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="p-0 btn">
+                            <img width="42" class="rounded-circle" src="assets/images/avatars/1.jpg" alt="">
+                            <i class="fa fa-angle-down ml-2 opacity-8"></i>
+                        </a>
+                        <div tabindex="-1" role="menu" aria-hidden="true" class="dropdown-menu dropdown-menu-right">
+                            <button type="button" tabindex="0" class="dropdown-item">User Account</button>
+                            <button type="button" tabindex="0" class="dropdown-item">Settings</button>
+                            <div tabindex="-1" class="dropdown-divider"></div>
+                            <button type="button" tabindex="0" class="dropdown-item">Log Out</button>
+                        </div>
+                    </div>
+                <div class="widget-content-right header-user-info ml-3">
+                </div>
+                
+            </div>
+        </div>
+    </div>        
+</div>
