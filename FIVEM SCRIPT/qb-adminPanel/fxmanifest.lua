02b3ac8a8fe319086f12ab183f80654d9a46ed21
aidanohart <44532573+aidanohartt@users.noreply.github.com>
2021-10-20 18:50:03 +0100

    Setup base files for the FiveM Script

---
+++ FIVEM SCRIPT/qb-adminPanel/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'A QBCore Administration Panel!'
+version '1.0.0'
+author 'aidanohart'
+
+shared_scripts 'config.lua'
+
+client_scripts {
+	'client/*.lua'
+}
+
+server_scripts  {
+    'server/*.lua'
+}
