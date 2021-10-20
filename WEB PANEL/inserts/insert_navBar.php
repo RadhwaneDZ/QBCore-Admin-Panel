5f6113e11fad71893565d70e583ce4d7613e7a8d
aidanohart <44532573+aidanohartt@users.noreply.github.com>
2021-10-21 00:39:10 +0100

    Altered navigation bar for new pages

--- WEB PANEL/inserts/insert_navBar.php
+++ WEB PANEL/inserts/insert_navBar.php
@@ -3,9 +3,26 @@
         <ul class="vertical-nav-menu">
         <li class="app-sidebar__heading">General</li>
             <li><a href="index.php" class="mm-active"><i class="metismenu-icon pe-7s-rocket"></i>Dashboard</a></li>
+            
+            <li class="app-sidebar__heading">Out-Of-Character Management</li>
+
+                <li><a href="playerSearch.php"><i class="metismenu-icon pe-7s-drawer"></i>Search All Players</a></li>
+
+                <li><a href="characterSearch.php"><i class="metismenu-icon pe-7s-drawer"></i>Current Online Players</a></li>
+
+                <li><a href="characterSearch.php"><i class="metismenu-icon pe-7s-diamond"></i>All Player Punishments<i class="metismenu-state-icon pe-7s-angle-down caret-left"></i></a>
+                    <ul>
+                        <li><a href="banlist.php"><i class="metismenu-icon"></i>Player Bans</a></li> 
+                        <li><a href="kicklist.php"><i class="metismenu-icon"></i>Player Kicks</a></li>  
+                        <li><a href="noteslist.php"><i class="metismenu-icon"></i>Player Notes</a></li>
+                    </ul>
+
             <li class="app-sidebar__heading">In-Character Management</li>
-            <li><a href="characterSearch.php"><i class="metismenu-icon pe-7s-drawer"></i>Search Characters</a></li>
-            <li><a href="vehicleSearch.php"><i class="metismenu-icon pe-7s-car"></i>Manage All Vehicles</a></li>
+
+                <li><a href="characterSearch.php"><i class="metismenu-icon pe-7s-drawer"></i>Search Characters</a></li>
+
+                <li><a href="vehicleSearch.php"><i class="metismenu-icon pe-7s-car"></i>Search Vehicles</a>
+            </li>
         </div>
     </div>
 </div>   
