6edbe56dbdce83d936461f70fca360b4d9942563
aidanohart <44532573+aidanohartt@users.noreply.github.com>
2021-10-21 08:45:31 +0100

    Inital fix of weird indents

--- WEB PANEL/inserts/insert_footer.php
+++ WEB PANEL/inserts/insert_footer.php
@@ -4,31 +4,17 @@ session_start();
 ?>
 
 <div class="app-wrapper-footer">
-
     <div class="app-footer">
-
         <div class="app-footer__inner">
-
             <div class="app-footer-right">
-
                 <ul class="nav">
-
                     <li class="nav-item">
-
                         <a href="https://github.com/aidanohart" class="nav-link">
-
                             QBCore Admin Panel - Made by aidanohart
-
                         </a>
-
                     </li>
-
                 </ul>
-
             </div>
-
         </div>
-
     </div>
-
 </div>    
