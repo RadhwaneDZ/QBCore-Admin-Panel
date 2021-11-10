f4966fa52cc7fbebb11545a4899ddbc9252a7811
aidanohart <44532573+aidanohartt@users.noreply.github.com>
2021-11-10 22:36:18 +0000

    Added database functionality for dashboard statistics

--- WEB PANEL/pages/page_dashboard.php
+++ WEB PANEL/pages/page_dashboard.php
@@ -1,10 +1,21 @@
+<?php
+try{
+    $database = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USERNAME, DB_PASSWORD);
+    $database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);} catch(PDOException $e) {echo "Connection failed: " . $e->getMessage();
+}
+$result = $database->query("SELECT count(*) FROM players");
+$num_rows = mysqli_fetch_row($result)[0];
+
+
+?>
+
 <div class="app-main__outer">
 <div class="app-main__inner">
     <div class="app-page-title">
         <div class="page-title-wrapper">
             <div class="page-title-heading">
                 <div class="page-title-icon">
-                    <i class="pe-7s-car icon-gradient qb-core">
+                    <i class="pe-7s-menu icon-gradient qb-core">
                     </i>
                 </div>
                 <div>Dashboard
@@ -41,7 +52,7 @@
                     <div class="widget-subheading">Number of unique players that have joined the server</div>
                 </div>
                 <div class="widget-content-right">
-                    <div class="widget-numbers text-danger">a number</div>
+                    <div class="widget-numbers text-danger"><?php echo $num_rows; ?></div>
                 </div>
             </div>
         </div>
