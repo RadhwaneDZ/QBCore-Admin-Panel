3d202a2ff53c7cc08eb33149e00922f6268ee970
aidanohart <44532573+aidanohartt@users.noreply.github.com>
2021-11-30 14:06:04 +0000

    Created functions so forms sync to database

--- WEB PANEL/pages/actions/page_newnote.php
+++ WEB PANEL/pages/actions/page_newnote.php
@@ -1,14 +1,21 @@
 <?php
-try{
-    $database = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USERNAME, DB_PASSWORD);
-    $database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);} catch(PDOException $e) {echo "Connection failed: " . $e->getMessage();
-}
-
 $playername = $_GET["playername"];
 $playerid = $_GET["playerid"];
 
+function createNote($punishedplayer, $punishedby, $punishedreason) {
+    $database = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USERNAME, DB_PASSWORD);
+    $database->query("INSERT INTO adminpanel_notes (punished_player, punished_by, punish_reason) VALUES ('$punishedplayer', '$punishedplayer','$punishedreason')");
+}
 
+if (isset($_POST['create_note']))
+{
+    $punishedplayer = htmlentities($_POST['player_id']);
+    $punishedby = htmlentities($_POST['player_name']);
+    $punishedreason = htmlentities($_POST['note_reason']);
 
+    // Create the note
+    createNote($punishedplayer, $punishedby, $punishedreason);
+}
 
 ?>
 
@@ -30,24 +37,26 @@ $playerid = $_GET["playerid"];
 </div>
 
 <div class="tab-content">
-<div class="tab-pane tabs-animation fade show active" id="tab-content-0" role="tabpanel">
+<div class="tab-pane tabs-animation fade show active" role="tabpanel">
     <div class="main-card mb-3 card">
         <div class="card-body"><h5 class="card-title">Player Info</h5>
-            <form class="">
-                <div class="position-relative row form-group"><label for="exampleEmail" class="col-sm-2 col-form-label">Player Name</label>
+            <form class="" method="post" action="#">
+
+            <div class="position-relative row form-group"><label class="col-sm-2 col-form-label">Player Name</label>
                     <div class="col-sm-10"><input class="form-control" name="player_name" value="<?php echo $playername ?>" readonly></div>
                 </div>
-                <div class="position-relative row form-group"><label for="exampleEmail" class="col-sm-2 col-form-label">Player ID</label>
+                
+                <div class="position-relative row form-group"><label class="col-sm-2 col-form-label">Player ID</label>
                     <div class="col-sm-10"><input class="form-control" name="player_id" value="<?php echo $playerid ?>" readonly></div>
                 </div>
+
                 <h5 class="card-title">Note Details</h5>
-                <div class="position-relative row form-group"><label for="exampleEmail" class="col-sm-2 col-form-label">Note Reason</label>
-                    <div class="col-sm-10"><input class="form-control" name="note_reason" placeholder="Remember to include your note reason!"></div>
+                <div class="position-relative row form-group"><label class="col-sm-2 col-form-label">Note Reason</label>
+                    <div class="col-sm-10"><input class="form-control" name="note_reason" required="text" placeholder="Remember to include your note reason!"></div>
                 </div>
-                <div class="position-relative row form-check">
-                    <div class="col-sm-10 offset-sm-2">
-                        <button class="btn btn-primary">Submit Note</button>
-                    </div>
+
+                <div class="col-sm-10 offset-sm-2">
+                    <button class="btn btn-primary" name="create_note">Submit Note</button>
                 </div>
             </form>
         </div>
