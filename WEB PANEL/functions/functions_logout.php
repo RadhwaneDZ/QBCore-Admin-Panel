7f71fbc03759885c287e642159c83cf3903029eb
aidanohart <44532573+aidanohartt@users.noreply.github.com>
2021-12-11 20:06:54 +0000

    Removed old functions

--- WEB PANEL/functions/functions_logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-require __DIR__ . "/functions_main.php";
-
-session_start();
-session_destroy(); # Closing the session and deleting all values associated with the session
-redirect("../index.php");
-?>
