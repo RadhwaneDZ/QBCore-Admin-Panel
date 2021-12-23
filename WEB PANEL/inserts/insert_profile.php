27dcf7ddddad0944b1d2ecf0080e0b3cec86b619
aidanohart <44532573+aidanohartt@users.noreply.github.com>
2021-12-23 02:24:02 +0000

    Removed old Discord profile header (pending)

--- WEB PANEL/inserts/insert_profile.php
+++ WEB PANEL/inserts/insert_profile.php
@@ -3,20 +3,20 @@
         <div class="widget-content p-0">
             <div class="widget-content-wrapper">
                 <div class="widget-content-left"> 
-                Logged in as: <b><?php echo $_SESSION['username'] . '#' . $_SESSION['discrim']; ?></b>
+                Logged in as: <b>Insert</b>
                 </div>
                   <div class="widget-content-right  ml-3 header-user-info">
                     <div class="widget-subheading">
                         <div class="btn-group">
                             <a data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="p-0 btn">
-                                <img width="42" class="rounded-circle" src="https://cdn.discordapp.com/avatars/<?php $extention = is_animated($_SESSION['user_avatar']);echo $_SESSION['user_id'] . "/" . $_SESSION['user_avatar'] . $extention; ?>" alt="">
+                                <img width="42" class="rounded-circle" src="https://cdn.discordapp.com/avatars/" alt="">
                                 <i class="fa fa-angle-down ml-2 opacity-8"></i>
                             </a>
                             <div tabindex="-1" role="menu" aria-hidden="true" class="dropdown-menu dropdown-menu-right">
                                 <button type="button" tabindex="0" class="dropdown-item">User Account</button>
                                 <button type="button" tabindex="0" class="dropdown-item">Settings</button>
                                 <div tabindex="-1" class="dropdown-divider"></div>
-                                <button type="button" tabindex="0" class="dropdown-item">Log Out</button>
+                                <button type="button" tabindex="0" class="dropdown-item"><a href="logout.php">Log Out</a></button>
                             </div>
                         </div>
                     </div>
